5703b5f26040332d0c14d14a55b9069ac65e071f
bcolemutech <bcolemutech@hotmail.com>
2020-08-30 22:13:25 -0500

    Send request with sdk

---
+++ dol-sdk-test/Controllers/AdminControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Http;
+using dol_sdk.Controllers;
+using dol_sdk.Enums;
+using dol_sdk.Services;
+using dol_sdk_test.TestHelpers;
+using Firebase.Auth;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using Xunit;
+
+namespace dol_sdk_test.Controllers
+{
+    public class AdminControllerTest
+    {
+        private readonly IAdminController _sut;
+        private readonly IHttpClientFactory _factory;
+        private readonly IConfiguration _configuration;
+        private readonly ISecurityService _securityService;
+        private FakeHttpMessageHandler _fakeHttpMessageHandler;
+
+        public AdminControllerTest()
+        {
+            _factory = Substitute.For<IHttpClientFactory>();
+            var configuration = Substitute.For<IConfiguration>();
+
+            _configuration = configuration;
+            _configuration["DolApiUri"].Returns("https://bogus.run.app/");
+
+            var security = Substitute.For<ISecurityService>();
+            var provider = Substitute.For<IFirebaseAuthProvider>();
+            _securityService = security;
+            _securityService.Identity.Returns(new FirebaseAuthLink(provider,
+                new FirebaseAuth
+                {
+                    FirebaseToken = "fakeToken",
+                    User = new User
+                    {
+                        LocalId = "12345"
+                    }
+                }));
+            _fakeHttpMessageHandler = new FakeHttpMessageHandler(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK
+            });
+            var fakeHttpClient = new HttpClient(_fakeHttpMessageHandler);
+
+            _factory.CreateClient().Returns(fakeHttpClient);
+
+            _sut = new AdminController(_factory, _configuration, _securityService);
+        }
+
+        [Fact]
+        public void CreateCharacterShouldSendPutRequestToCharacterWithName()
+        {
+            _sut.UpdateUser("Jake@test.com", Authority.Player);
+
+            _fakeHttpMessageHandler.RequestMessage.Method.Should().Be(HttpMethod.Post);
+            _fakeHttpMessageHandler.RequestMessage.RequestUri.Should().Be("https://bogus.run.app/user");
+            _fakeHttpMessageHandler.RequestMessage.Headers.Authorization.Scheme.Should().Be("Bearer");
+            _fakeHttpMessageHandler.RequestMessage.Headers.Authorization.Parameter.Should().Be("fakeToken");
+            _fakeHttpMessageHandler.RequestMessage.Content.ReadAsStringAsync().Result.Should()
+                .Be("{\"Email\":\"Jake@test.com\",\"Authority\":2}");
+        }
+    }
+}
--- dol-sdk/Controllers/AdminController.cs
+++ dol-sdk/Controllers/AdminController.cs
@@ -1,4 +1,10 @@
-﻿using dol_sdk.Enums;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using dol_sdk.Enums;
+using dol_sdk.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace dol_sdk.Controllers
 {
@@ -9,9 +15,44 @@ namespace dol_sdk.Controllers
 
     public class AdminController : IAdminController
     {
+        private const string Bearer = "Bearer";
+        private readonly ISecurityService _security;
+        private readonly HttpClient _client;
+        private readonly string _requestUri;
+        public AdminController(IHttpClientFactory factory, IConfiguration configuration, ISecurityService security)
+        {
+            _security = security;
+            _client = factory.CreateClient();
+            _requestUri = configuration["DolApiUri"] + "user";
+        }
+        
+        private string IdToken => _security.Identity.FirebaseToken;
+
         public void UpdateUser(string email, Authority authority)
         {
-            throw new System.NotImplementedException();
+            var content = new PlayerRequest(email, authority);
+
+            var contentJson = JsonConvert.SerializeObject(content); 
+
+            var request = new HttpRequestMessage(HttpMethod.Post, _requestUri);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue(Bearer, IdToken);
+            request.Content = new StringContent(contentJson);
+            
+            var response = _client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
         }
+    }
+
+    public readonly struct PlayerRequest
+    {
+        public PlayerRequest(string email, Authority authority)
+        {
+            Email = email;
+            Authority = authority;
+        }
+
+        public string Email { get; }
+        public Authority Authority { get; }
     }
 }
